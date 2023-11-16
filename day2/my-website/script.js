15d931ed09cc62765e4af92f6879fbdc4e72c4a0
Zareef Ahmed <zareef@zareef.com>
2023-11-16 12:54:29 +0530

    Added code examples

---
+++ day2/my-website/script.js
@@ -0,0 +1,31 @@
+function saySomething()
+{
+
+
+}
+
+sayOtherthing=function(){
+    console.log("I was called by otherthing");
+}
+
+sayOtherthing();
+
+
+var fname="Zareef";
+let lname="Ahmed";
+const PI=3.14
+
+function sayName()
+{
+  //  PI=4.5;
+let fname="Ahmed";
+let lname="Zareef"
+
+  //  console.log(name);
+}
+
+sayName(); /// >>> 
+
+console.log(PI);
+//console.log(fname); ///<<< 
+//console.log(lname); ///<<< 
---
+++ day2/my-website/script2.js
@@ -0,0 +1,7 @@
+
+
+    function saySomething()
+    {
+        /// Do nothing
+        console.log("Function saySomething was called.")
+    }
