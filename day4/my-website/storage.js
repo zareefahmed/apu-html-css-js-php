c956f4ddf0384c3dca34ac6b9f8cc7c3561f7d15
Zareef Ahmed <zareef@zareef.com>
2023-11-20 08:07:21 +0530

    Added example and assignment

---
+++ day4/my-website/storage.js
@@ -0,0 +1,22 @@
+class myStorage{
+
+    constructor()
+    {
+        this.storageclass=localStorage;
+    }
+
+    setItem(itemName,ItemValue)
+    {
+
+        this.storageclass.setItem(itemName,ItemValue);
+    }
+
+    getItem(itemName){
+        console.log(this.storageclass);
+     return   this.storageclass.getItem(itemName);
+    }
+
+    setStorageType(storagetype){
+        this.storageclass=storagetype;
+    }
+}
