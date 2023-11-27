7499085024b8963917eca5f14a6576f91f09cc62
Zareef Ahmed <zareef@zareef.com>
2023-11-27 12:02:54 +0530

    Added project code

--- project/api/common.php
+++ project/api/common.php
@@ -2,11 +2,18 @@
 
 function make_response($data,$status="failure")
 {
+ 
     $defultvalues=array(
         "version"=>"1.0",
-        "action"=>ACTION,
         "status"=>$status
     );
+    if(defined("ACTION"))
+    {
+        $defultvalues["action"]=ACTION;
+    }
+   
+
+
 
     $result= array_merge($defultvalues,$data);
     print json_encode($result);
